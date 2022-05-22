4a41c53e84735ba20b4b24e9cf847c11eba74fef
Stephane Mensah <waptik@users.noreply.github.com>
2022-05-22 09:12:24 +0000

    built on -> built by

--- pages/index.js
+++ pages/index.js
@@ -47,7 +47,7 @@ const App = () => {
             href={TWITTER_LINK}
             target="_blank"
             rel="noreferrer"
-          >{`built on @${TWITTER_HANDLE}`}</a>
+          >{`built by @${TWITTER_HANDLE}`}</a>
         </div>
       </div>
     </div>
